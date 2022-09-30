70b2cfff1e49e6fb42c0cb97d4eb4ae46ecedf93
Kim Seung Gyeom <74819625+k2h2j3@users.noreply.github.com>
2022-09-30 19:10:18 +0900

    Add files via upload

--- pyramid.py
+++ pyramid.py
@@ -1,12 +1,15 @@
 class Pyramid:
+    #피라미드 생성
     def __init__(self, height):
         self.height = height
+    # 높이값 수정
     def set(self, height):
         self.height = height
+    # 생성된 피라미드 출력
     def show(self):
         for i in range(1, self.height+1):
             print(' ' * (self.height - i) + '*' * (i * 2 - 1))
-
+    # 피라미드 블록 개수
     def getBlock(self):
         total = 0
         for i in range(1, self.height+1):

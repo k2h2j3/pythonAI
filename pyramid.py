d775f92b46b29999c8f32247877ba82167f90451
Kim Seung Gyeom <74819625+k2h2j3@users.noreply.github.com>
2022-09-30 19:07:52 +0900

    Add files via upload

--- pyramid.py
+++ pyramid.py
@@ -1,8 +1,17 @@
-def pyramid(num):
-    for i in range(1, num + 1):
-        result = ' ' * (num - i) + '*' * ((2 * i) - 1)
-        print(result)
+class Pyramid:
+    def __init__(self, height):
+        self.height = height
+    def set(self, height):
+        self.height = height
+    def show(self):
+        for i in range(1, self.height+1):
+            print(' ' * (self.height - i) + '*' * (i * 2 - 1))
 
-n = int(input('floor'))
-pyramid(n)
+    def getBlock(self):
+        total = 0
+        for i in range(1, self.height+1):
+            total = total + (i * 2 - 1)
+        return total
 
+p = Pyramid(5)
+p.show()
